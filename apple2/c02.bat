dfedddab72025534bdb61140c9684887a0001f50
Curtis F Kaylor <revcurtis@gmail.com>
2018-02-17 13:52:13 -0500

    Renamed Apple 2 standard header files

--- apple2/c02.bat
+++ apple2/c02.bat
@@ -1,5 +1,16 @@
-@ECHO Compiling File %1.c02
-..\c02.exe %1
+@echo Compiling File %1.c02
+..\c02.exe %1 >%1.out
+@if ERRORLEVEL 1 exit /b
 
-@ECHO Assembling File %1.asm
-C:\Programs\dasm %1.asm -f3 -o%1.bin -l%1.lst -s%1.sym
+@echo Assembling File %1.asm
+dasm %1.asm -f3 -o%1.bin -l%1.lst -s%1.sym
+@if ERRORLEVEL 1 exit /b
+
+@echo Building Disk Image
+copy /Y boot.dsk %1.dsk
+echo BRUN %1 |a2tools in t %1.dsk command
+a2tools in b.0C00 %1.dsk %1 %1.bin
+@if ERRORLEVEL 1 exit /b
+
+@echo Starting Emulator
+start C:\Programs\AppleWin\applewin -d1 %1.dsk 
