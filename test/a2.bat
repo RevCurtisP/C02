35377b58074efa4d937449fa3d1b6a64571a531a
Curtis F Kaylor <revcurtis@gmail.com>
2018-09-12 09:54:54 -0400

    Squashed commit of the following:
    commit ed00e1d1b5a9783a72dade3f3676b161a9cfe287
    Author: Curtis F Kaylor <revcurtis@gmail.com>
    Date:   Sun Sep 9 22:20:49 2018 -0400
    
        Documented joystk, paddle, and lgtpen modules
    
    commit ec0a5ede8d1b043fcf0094ea653255a808dbf8d3
    Author: Curtis F Kaylor <revcurtis@gmail.com>
    Date:   Sun Sep 9 20:31:11 2018 -0400
    
        Added joystick, paddle, and lightpen test programs
    
    commit 7b787f432e2f4f7ae5d7f0053ade1d3586a4fad1
    Author: Curtis F Kaylor <revcurtis@gmail.com>
    Date:   Sun Sep 9 20:30:03 2018 -0400
    
        Updated Apple II and VIC-20 Batch Files
    
    commit 50568294349d7e3c6b7d0d364aeaece73c9e4ab6
    Author: Curtis F Kaylor <revcurtis@gmail.com>
    Date:   Sun Sep 9 20:28:09 2018 -0400
    
        Separated light pen code into separate files
    
    commit d45e59f73d55eef1d30c591d19a043ad79cfd81a
    Author: Curtis F Kaylor <revcurtis@gmail.com>
    Date:   Sun Sep 9 19:28:56 2018 -0400
    
        Moved code for paddles into separate include files
    
    commit fc5c5472d758c960332ea14105d5ec4a7c8cbbfb
    Author: Curtis F Kaylor <revcurtis@gmail.com>
    Date:   Sun Sep 9 16:15:32 2018 -0400
    
        Added system specific module 'joystk'

--- test/a2.bat
+++ test/a2.bat
@@ -5,7 +5,7 @@ IF EXIST %1.c02 GOTO COMPILE
 
 :COMPILE
 ECHO Compiling File %1.c02
-..\c02.exe -h apple2 %1 >%1.dbg
+..\c02.exe -h apple2 -s apple2 %1 >%1.dbg
 IF ERRORLEVEL 1 EXIT /B
 
 ECHO Assembling File %1.asm
