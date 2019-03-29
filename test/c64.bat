33a19c7a873788cad2debd241e9bb17eaa6109ad
Curtis F Kaylor <revcurtis@gmail.com>
2019-03-29 19:09:04 -0400

    Added test/fileread.c02 for VIC-20 and C64

--- test/c64.bat
+++ test/c64.bat
@@ -5,7 +5,7 @@ IF EXIST %1.c02 GOTO COMPILE
 
 :COMPILE
 ECHO Compiling File %1.c02 for Commodore 64
-..\c02.exe -h c64 -s c64 %1 >%1.dbg
+..\c02.exe -h c64 -s c64 -s cbm %1 >%1.dbg
 IF %ERRORLEVEL% NEQ 0 GOTO EOF
 ECHO Assembling File %1.asm
 C:\Programs\dasm %1.asm -f1 -o%1.prg -l%1.lst -s%1.sym
--- test/v8k.bat
+++ test/v8k.bat
@@ -5,7 +5,7 @@ IF EXIST %1.c02 GOTO COMPILE
 
 :COMPILE
 ECHO Compiling File %1.c02 for VIC 20 +8k
-..\c02.exe -h vic8k -s vic %1 >%1.dbg
+..\c02.exe -h vic8k -s vic -s cbm %1 >%1.dbg
 IF %ERRORLEVEL% NEQ 0 GOTO EOF
 ECHO Assembling File %1.asm
 C:\Programs\dasm %1.asm -f1 -o%1.prg -l%1.lst -s%1.sym
@@ -13,6 +13,6 @@ C:\Programs\dasm %1.asm -f1 -o%1.prg -l%1.lst -s%1.sym
 IF %ERRORLEVEL% NEQ 0 GOTO EOF
 
 ECHO Starting Emulator
-@start C:\Programs\WinVICE\xvic.exe -config xvic.ini %1.prg
+@start C:\Programs\WinVICE\xvic.exe -config xvic.ini -1 testtape.t64 -10 geosdisk.d64 %1.prg
 
 :EOF
