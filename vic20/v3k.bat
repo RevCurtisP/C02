37b98c5d0bc4011b1ed429a375c74224d0c5b3df
Curtis F Kaylor <revcurtis@gmail.com>
2020-09-08 13:02:43 -0400

    Modified vic20/*.bat to use A02 assembler

--- vic20/v3k.bat
+++ vic20/v3k.bat
@@ -8,7 +8,7 @@ ECHO Compiling File %1.c02 for VIC 20 +8k
 ..\c02.exe -d -h vic3k -s vic %1 >%1.dbg
 IF %ERRORLEVEL% NEQ 0 GOTO EOF
 ECHO Assembling File %1.asm
-C:\Programs\dasm %1.asm -f1 -o%1.prg -l%1.lst -s%1.sym
+..\a02.exe -p %1.asm %1.prg %1.lst
 
 IF %ERRORLEVEL% NEQ 0 GOTO EOF
 
