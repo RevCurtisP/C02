510b44da918294dabc680247da7ede1d4355e360
Curtis F Kaylor <revcurtis@gmail.com>
2020-09-08 11:57:50 -0400

    Fixed vic3k.h02, updated vic20 batch files

---
+++ vic20/v8k.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+IF EXIST %1.c02 GOTO COMPILE
+    ECHO File %1.c02 not found
+    GOTO EOF
+
+:COMPILE
+ECHO Compiling File %1.c02 for VIC 20 +8k
+..\c02.exe -d -h vic8k -s vic %1 >%1.dbg
+IF %ERRORLEVEL% NEQ 0 GOTO EOF
+ECHO Assembling File %1.asm
+C:\Programs\dasm %1.asm -f1 -o%1.prg -l%1.lst -s%1.sym
+
+IF %ERRORLEVEL% NEQ 0 GOTO EOF
+
+ECHO Starting Emulator
+@start C:\Programs\WinVICE\xvic.exe -config xvic.ini -1 testtape.t64 -10 testdisk.d64 %1.prg
+
+:EOF
